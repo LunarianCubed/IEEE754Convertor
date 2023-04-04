c37c2bec5aa36582b1335a3b74d50a39955b0f8e
Lunarian3 <71202068+LunarianCubed@users.noreply.github.com>
2023-04-04 07:45:27 -0500

    03 Night

--- src/Convertor.java
+++ src/Convertor.java
@@ -3,13 +3,17 @@ public class Convertor {
         * This is a class that converts a float to it's IEEE 754 binary representation.
      */
 
-    static StringBuilder sb = new StringBuilder();
+    static StringBuilder sb32 = new StringBuilder();
+    static StringBuilder sb64 = new StringBuilder();
     static int intBits;
     public static void Convert(double num) {
         if(num < 0){
-            sb.append(1);
+            sb32.append(1);
+            sb64.append(1);
+            num *= -1;
         }else {
-            sb.append(0);
+            sb32.append(0);
+            sb64.append(0);
         }
         intBits = (int)num;
 
@@ -19,19 +23,19 @@ public class Convertor {
 //        System.out.println(binary);
     }
 
-    private static void convertInteger(int num, int bias){
-
-
+    private static void convertInteger(int num){
+        int remainder32 = 2;
+        int remainder64 = 2;
     }
     private static void convertFractional(double num){
         double fractional = num;
         while(fractional != 0){
             fractional *= 2;
             if(fractional >= 1){
-                sb.append(1);
+                sb32.append(1);
                 fractional -= 1;
             }else{
-                sb.append(0);
+                sb32.append(0);
             }
         }
     }
